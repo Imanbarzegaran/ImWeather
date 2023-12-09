d92a20d07bb1b2c6897955f8d5685548a7c978fc
iman barzegaran <i.barzegaran@gmail.com>
2023-12-09 19:24:36 +0330

    start project

--- ImWeather/Controller/WeatherViewController.swift
+++ ImWeather/Controller/WeatherViewController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  WeatherViewController.swift
 //  ImWeather
 //
 //  Created by MacBook Air on 12/9/23.
@@ -7,7 +7,7 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class WeatherViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
---
+++ ImWeather/Model/WeatherManager.swift
@@ -0,0 +1,8 @@
+//
+//  WeatherManager.swift
+//  ImWeather
+//
+//  Created by MacBook Air on 12/9/23.
+//
+
+import Foundation
