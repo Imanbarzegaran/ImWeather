e6cd6475d2abfbd700cd8477337605f6d515a557
iman barzegaran <i.barzegaran@gmail.com>
2023-12-09 20:47:12 +0330

    insert IBOtlets

--- ImWeather/Controller/WeatherViewController.swift
+++ ImWeather/Controller/WeatherViewController.swift
@@ -6,12 +6,31 @@
 //
 
 import UIKit
+import CoreLocation
 
 class WeatherViewController: UIViewController {
+    
+    @IBOutlet weak var locationLabel: UILabel!
+    @IBOutlet weak var conditionLabel: UILabel!
+    @IBOutlet weak var unitLabel: UILabel!
+    @IBOutlet weak var tempLabel: UILabel!
+    @IBOutlet weak var conditionIcon: UIImageView!
+    @IBOutlet weak var next7DayButton: UIButton!
+    @IBOutlet weak var conditionImage: UIImageView!
+    @IBOutlet weak var searchTextField: UITextField!
+    
+    
+    
+    
+    let locationManager = CLLocationManager()
+    
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        
     }
 
 
