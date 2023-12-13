7be1211089bc53b3c0412af01d0e7957e35279dd
iman barzegaran <i.barzegaran@gmail.com>
2023-12-13 17:48:42 +0330

    set protocol and urlString for networking step in WeatherManager

--- ImWeather/Model/WeatherManager.swift
+++ ImWeather/Model/WeatherManager.swift
@@ -6,3 +6,20 @@
 //
 
 import Foundation
+
+protocol WeatherManagerDelegate {
+    func didFaileWithError (error: Error)
+    func didUpdateWeather (_ weatherManager: WeatherManager, weather: WeatherModel)
+}
+
+struct WeatherManager {
+    let weatherURL = "https://api.openweathermap.org/data/2.5/weather?appid=d5e422e0aa5a9636a759208fe9de1eb2&units=metric"
+    var delegte: WeatherManagerDelegate?
+    
+    func fetchWeather (city: String) {
+        let URLString = "\(weatherURL)&q=\(city)"
+        performRequest(with: URLString)
+    }
+    
+    
+}
